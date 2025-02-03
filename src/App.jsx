0fa3103d737e479aaf9070773621e346f84f4dbe
Pundlik Mane <asmita.pundlik.mane@accenture.com>
2025-02-03 11:42:49 +0530

    Create app and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import React from 'react'
+function App() {
+
+  return (
+    <>
+      <h1 className='text-2xl font-bold text-red-500'>Hello world</h1>
+    </>
+  )
+}
+
+export default App
